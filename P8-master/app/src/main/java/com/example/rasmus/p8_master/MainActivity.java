08145610941ffa390b623fe87bde5667ecd51402
Rasmus <rrosen13@student.aau.dk>
2017-03-26 12:36:10 +0200

    New classes
    I have made "Card" as an abstract super class and the individual cards
    as subclasses. I need to make an object of each class (with the image as
    an attribute) and make an array with these objects which then can be
    loaded into the listView.

--- P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
+++ P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.rasmus.p8_master;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static com.example.rasmus.p8_master.R.id.listOfCards;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is just to test the branch!!
+        /** This is what Maja did. I need to implement my part in this!
+        ListView lv = (ListView) findViewById(listOfCards);
+        final String [] values = new String[]{"den første", "den anden"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.activity_list_item, android.R.id.text1, values);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id){
+                if (position == 0) {
+                    Intent myintent = new Intent (view.getContext(), Activity1.class);
+                    startActivityForResult(myintent,0);
+                }
+                if (position == 1){
+                    Intent myintent = new Intent (view.getContext(), Activity2.class);
+                    startActivityForResult(myintent,1);
+                }
+            }
+        }); */
     }
+
 }
