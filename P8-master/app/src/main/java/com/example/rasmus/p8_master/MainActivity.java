a46ad9c3bd3dbc421b2a37bf556d2bc3d71fdaa4
Rasmus <rrosen13@student.aau.dk>
2017-03-29 12:43:45 +0200

    listView with images
    The listView is working now with all the images in it. And they are
    clickable!

--- P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
+++ P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
@@ -1,41 +1,41 @@
 package com.example.rasmus.p8_master;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import java.util.ArrayList;
 
-import static com.example.rasmus.p8_master.R.id.listOfCards;
+import android.os.Bundle;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    Context context;
+
+    ArrayList imgCards;
+    public static int [] images ={R.drawable.img_rejsekort, R.drawable.img_koerekort, R.drawable.img_sundhedskort,
+                                  R.drawable.img_creditcard, R.drawable.img_ungdomskort};
+    public static String [] prgmNameList={"Let Us C", "hejej","hehrej", "hehej", "jugjug"};
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /** This is what Maja did. I need to implement my part in this!
-        ListView lv = (ListView) findViewById(listOfCards);
-        final String [] values = new String[]{"den første", "den anden"};
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.activity_list_item, android.R.id.text1, values);
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id){
-                if (position == 0) {
-                    Intent myintent = new Intent (view.getContext(), Activity1.class);
-                    startActivityForResult(myintent,0);
-                }
-                if (position == 1){
-                    Intent myintent = new Intent (view.getContext(), Activity2.class);
-                    startActivityForResult(myintent,1);
-                }
-            }
-        }); */
+        context=this;
+
+        lv=(ListView) findViewById(R.id.listOfCards);
+        lv.setAdapter(new CustomAdapter(this, prgmNameList, images));
+
     }
+
+   /** @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
 
 }
--- P8-master/app/src/main/java/com/example/rasmus/p8_master/Rejsekort.java
+++ P8-master/app/src/main/java/com/example/rasmus/p8_master/Rejsekort.java
@@ -1,8 +1,11 @@
 package com.example.rasmus.p8_master;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by Rasmus on 26-03-2017.
  */
 
 public class Rejsekort extends Card {
+
 }
