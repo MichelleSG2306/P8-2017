31d5b2ce49ba851b54ef75e38ebce9d33015397e
chatzilbabis <cchatz16@student.aau.dk>
2017-04-03 11:24:23 +0200

    test
    test

--- P8-master/app/src/main/java/com/example/rasmus/p8_master/Sundhedskort.java
+++ P8-master/app/src/main/java/com/example/rasmus/p8_master/Sundhedskort.java
@@ -1,8 +1,13 @@
 package com.example.rasmus.p8_master;
 
-/**
- * Created by Rasmus on 26-03-2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Sundhedskort extends Card {
+public class Sundhedskort extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sundhedskort);
+    }
 }
