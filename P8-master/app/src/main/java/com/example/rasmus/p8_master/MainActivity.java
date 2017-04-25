d3b6f083ce46d5ce3edd2bf99e834e181976b3be
Rasmus <rrosen13@student.aau.dk>
2017-04-25 13:42:45 +0200

    back to master
    Should be back to normal now

--- P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
+++ P8-master/app/src/main/java/com/example/rasmus/p8_master/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.rasmus.p8_master;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+//import org.apache.http.NameValuePair;
+//import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static int [] images ={R.drawable.rejsekort_f, R.drawable.driverslicence_f, R.drawable.sundhedskort_f,
                                   R.drawable.bankcard_f, R.drawable.ungdomskort_f};
+
 
 
     @Override
@@ -35,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-
         });
     }
+
 }
 
